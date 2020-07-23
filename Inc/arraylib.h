84a5944741db861dc7450ba610045e424c0ad4a9
soyfish <xyj597@outlook.com>
2020-07-24 00:31:39 +0800

    first commit

---
+++ Inc/arraylib.h
@@ -0,0 +1,13 @@
+#ifndef __LIB_H
+#define __LIB_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <string.h>
+
+extern uint8_t *Check_Buffer(uint8_t *buff, uint16_t buff_len, uint8_t *data, uint16_t data_len);
+
+
+
+#endif // __LIB_H
---
+++ Src/arraylib.c
@@ -0,0 +1,46 @@
+#include "arraylib.h"
+
+
+/******************************************************
+* 检测数组 data 是否是数组 buff 的子集
+* 是则返回 buff 中 data 第一次出现的地址
+* 不是则返回 NULL
+******************************************************/
+uint8_t *Check_Buffer(uint8_t *buff, uint16_t buff_len, uint8_t *data, uint16_t data_len)
+{
+  uint16_t head, end;
+  uint8_t *ptr = NULL;
+
+  if (buff_len >= data_len)
+  {
+    for (head = 0; head < buff_len; head++)
+    {
+      if (NULL == ptr)
+      {
+        if (head + data_len <= buff_len)
+        {
+          if (*(buff + head) == *data)
+            ptr = buff + head;
+        }
+        else  return NULL;
+      }
+      else
+      {
+        for (end = 0; end < data_len; end++)
+        {
+          if (*(ptr + end) != *(data + end))
+          {
+            ptr = NULL;
+            break;
+          }
+        }
+        if (end == data_len)
+          return ptr;
+      }
+    }
+  }
+
+  return NULL;
+}
+
+
